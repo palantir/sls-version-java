b24e8d7f70856da198822f8426f768799590b013
Patrick Koenig <pkoenig@palantir.com>
2019-09-09 12:40:55 -0700

    Change ImmutablesStyle package

--- sls-versions/src/main/java/com/palantir/sls/versions/ImmutablesStyle.java
+++ sls-versions/src/main/java/com/palantir/sls/versions/ImmutablesStyle.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.palantir.immutables.style;
+package com.palantir.sls.versions;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
--- sls-versions/src/main/java/com/palantir/sls/versions/NonOrderableSlsVersion.java
+++ sls-versions/src/main/java/com/palantir/sls/versions/NonOrderableSlsVersion.java
@@ -20,7 +20,6 @@ import static com.palantir.logsafe.Preconditions.checkArgument;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
-import com.palantir.immutables.style.ImmutablesStyle;
 import com.palantir.logsafe.UnsafeArg;
 import java.util.Optional;
 import java.util.regex.Matcher;
